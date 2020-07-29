ec3610363c9e45adc49b3defb5cfe2d5634c4b4f
jbalpert <jbalpert@umich.edu>
2020-07-29 11:06:09 -0400

    fixed the local storage bug!

--- assets/JS/TodoList.js
+++ assets/JS/TodoList.js
@@ -49,6 +49,7 @@ const ul = document.querySelector("ul");
 	else if(click === "i"){
 		deleteTodo(event.target.parentNode.parentNode);
 	}
+	storeTodos();
 });
 
 //Add a todo
@@ -58,10 +59,16 @@ addTodo(textbox);
 
 function deleteTodo(removeParent){
 	let index = (Array.prototype.indexOf.call(removeParent.parentNode.children, removeParent));
+	console.log(localStorage);
 	deleteStorage(index);
+	console.log(localStorage);	
 	removeParent.classList.add("hidden");
 	event.stopPropagation();
-	setTimeout(function(){removeParent.remove();}, 500);
+	setTimeout(function(){
+		removeParent.remove();
+		storeTodos();
+		}
+	, 500);
 }
 
 function addTodo(textbox){
@@ -92,13 +99,14 @@ function storeTodos(){
 }
 
 function deleteStorage(index){
-	storeTodos();
 	localStorage.removeItem(index.toString());
 }
 
 function reloadTodos(){
+	console.log(localStorage);
 	for (let i = 0; i < localStorage.length; i++){
 		let todo = localStorage.getItem(i.toString());
+		console.log(localStorage.getItem(i.toString()));
 		ul.innerHTML += "<li><span><i class=\"fa fa-trash\" aria-hidden=\"true\"></i></span> " + todo + "</li>";
 	}
 }
